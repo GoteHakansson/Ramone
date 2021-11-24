5bdac7063bbd6eb4df6be15dcfa5bbabc1fc42ea
Göte Håkansson <gote.hakansson@bahnhof.se>
2021-11-24 14:38:02 +0100

    Updated source code.

--- Code/libSocketCan/include/socketcan.hpp
+++ Code/libSocketCan/include/socketcan.hpp
@@ -25,7 +25,6 @@ namespace scpp
         uint8_t len = 0;
         uint8_t flags = 0;
         uint8_t data[64];
-
     };
 
     enum SocketCanStatus
@@ -33,8 +32,8 @@ namespace scpp
         STATUS_OK = 1 << 0,
         STATUS_SOCKET_CREATE_ERROR = 1 << 2,
         STATUS_INTERFACE_NAME_TO_IDX_ERROR = 1 << 3,
-        STATUS_MTU_ERROR = 1 << 4, /// maximum transfer unit
-        STATUS_CANFD_NOT_SUPPORTED = 1 << 5, /// Flexible data-rate is not supported on this interface
+        STATUS_MTU_ERROR = 1 << 4,               /// maximum transfer unit
+        STATUS_CANFD_NOT_SUPPORTED = 1 << 5,     /// Flexible data-rate is not supported on this interface
         STATUS_ENABLE_FD_SUPPORT_ERROR = 1 << 6, /// Error on enabling fexible-data-rate support
         STATUS_WRITE_ERROR = 1 << 7,
         STATUS_READ_ERROR = 1 << 8,
@@ -46,14 +45,16 @@ namespace scpp
     public:
         SocketCan();
         SocketCan(const SocketCan &) = delete;
-        SocketCan & operator=(const SocketCan &) = delete;
-        SocketCanStatus open(const std::string & can_interface, int32_t read_timeout_ms = 3, SocketMode mode = MODE_CAN_MTU);
-        SocketCanStatus write(const CanFrame & msg);
-        SocketCanStatus read(CanFrame & msg);
+        SocketCan &operator=(const SocketCan &) = delete;
+        SocketCanStatus open(const std::string &can_interface, int32_t read_timeout_ms = 3, SocketMode mode = MODE_CAN_MTU);
+        SocketCanStatus write(const CanFrame &msg);
+        SocketCanStatus read(CanFrame &msg);
         SocketCanStatus close();
         void send(int data[], int length);
-        const std::string & interfaceName() const;
+        void send(int data[], const int n, const int id);
+        const std::string &interfaceName() const;
         ~SocketCan();
+
     private:
         int m_socket = -1;
         int32_t m_read_timeout_ms = 3;
